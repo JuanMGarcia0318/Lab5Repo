717cb7b339498973ece240951341e795f07ef4fb
Juan M. Garcia <main@Juans-MacBook-Pro.local>
2018-02-18 19:05:15 -0500

    hello

--- Test 1.0/src/Main.java
+++ Test 1.0/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("Hello world!!");
-        System.out.println("My name is Adam");
+        System.out.println("My name is Adam"); // This was a change for the commit
+        System.out.println("But you can call me A");
     }
 }
